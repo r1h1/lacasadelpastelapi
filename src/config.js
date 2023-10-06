087e3b2735ecfd4867712b856eff188f9670f3df
Daniel Rivas <de.rivasherrera@gmail.com>
2023-10-06 14:35:27 -0600

    Chansessdfjskdfjlasj

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 //SE EXPORTA EL PUERTO Y LOS DATOS DE CONEXIÓN
 module.exports = {
     app: {
-        port: process.env.port || 3000,
+        port: 3000,
     },
     jwt: {
-        secret: process.env.jet_secret || '7l;=e?H~*K(^XiR',
+        secret: '7l;=e?H~*K(^XiR',
     },
     mysql: {
-        host: process.env.mysql_host || 'containers-us-west-53.railway.app',
-        user: process.env.mysql_user || 'root',
-        password: process.env.mysql_password || 'gIl8DVcRfYNRKdxnXRty',
-        database: process.env.mysql_db || 'railway',
-        port: process.env.mysql_port || 7959
+        host: 'containers-us-west-53.railway.app',
+        user: 'root',
+        password: 'gIl8DVcRfYNRKdxnXRty',
+        database: 'railway',
+        port: 7959
     }
 }
