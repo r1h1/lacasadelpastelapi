4c6bb891d4cb8559f84e72a11b00bba5912c4fb4
Daniel Rivas <de.rivasherrera@gmail.com>
2023-10-06 14:07:37 -0600

    Se configura

--- src/config.js
+++ src/config.js
@@ -7,9 +7,10 @@ module.exports = {
         secret: process.env.jet_secret || '7l;=e?H~*K(^XiR',
     },
     mysql: {
-        host: process.env.mysql_host || 'localhost',
+        host: process.env.mysql_host || 'containers-us-west-53.railway.app',
         user: process.env.mysql_user || 'root',
-        password: process.env.mysql_password || '',
-        database: process.env.mysql_db || 'bakerygo',
+        password: process.env.mysql_password || 'gIl8DVcRfYNRKdxnXRty',
+        database: process.env.mysql_db || 'railway',
+        port: process.env.mysql_port || '7959'
     }
 }
